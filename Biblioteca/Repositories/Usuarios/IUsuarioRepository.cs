d52b60dd5cc4fd0cbada80221074d1a37e10a3ce
Eduardo <eduardo.olvreis@gmail.com>
2026-04-02 13:46:19 -0300

    feat (UsuarioRepository): Implementação da lógica dos repositorys do Usuario

---
+++ Biblioteca/Repositories/Usuarios/IUsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using Biblioteca.Models;
+
+namespace Biblioteca.Repositories.Usuarios
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> CriarUsuarioAsync(Usuario usuario);
+        Task<Usuario?> ObterPorEmailAsync(string email);
+    }
+}
---
+++ Biblioteca/Repositories/Usuarios/SqlUsuarioRepository.cs
@@ -0,0 +1,29 @@
+﻿using Biblioteca.Data;
+using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Repositories.Usuarios
+{
+    public class SqlUsuarioRepository : IUsuarioRepository
+    {
+        private readonly AppDbContext _context;
+        public SqlUsuarioRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Usuario> CriarUsuarioAsync(Usuario usuario)
+        {
+            _context.Add(usuario);
+            await _context.SaveChangesAsync();
+            return usuario;
+        }
+
+        public async Task<Usuario?> ObterPorEmailAsync(string email)
+        {
+            var usuarioEncontrado = await _context.Usuarios.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email);
+            if(usuarioEncontrado == null) { return null; };
+            return (usuarioEncontrado);
+        }
+    }
+}
