ee88697cdabb1d276c1a5ee9f50b2211c1dce5cb
Eduardo <eduardo.olvreis@gmail.com>
2026-04-02 11:51:27 -0300

    feat (UsuarioDTOs): Implementação da UsuarioResponseDto e UsuarioRegistroDto

---
+++ Biblioteca/DTOs/Usuarios/UsuarioRegistroDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.DTOs.Usuarios
+{
+    public class UsuarioRegistroDto
+    {
+        [Required(ErrorMessage = "Campo 'Nome' não pode ser vazio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Email' não pode ser vazio")]
+        [EmailAddress(ErrorMessage = "Formato de Email inválido")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Senha { get; set; }
+    }
+}
---
+++ Biblioteca/DTOs/Usuarios/UsuarioResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Biblioteca.DTOs.Usuarios
+{
+    public class UsuarioResponseDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
