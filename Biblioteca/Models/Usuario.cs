a54c87620ad3d6601c983e9ebd279b957162272f
Eduardo <eduardo.olvreis@gmail.com>
2026-04-02 11:33:51 -0300

    feat (Model Usuario): Criação da model de usuário para login e registro

---
+++ Biblioteca/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Nome' não pode ser vazio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo 'Email' não pode ser vazio")]
+        [EmailAddress(ErrorMessage = "Formato de Email inválido")]
+        public string Email { get; set; }
+
+        [Required]
+        public string SenhaHash { get; set; }
+    }
+}
